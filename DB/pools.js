c3342f7fcdd155bbd42e4499452375dd92583e40
sami7453 <145992280+sami7453@users.noreply.github.com>
2025-05-22 22:57:19 +0200

    Update pools.js for deployment

--- DB/pools.js
+++ DB/pools.js
@@ -1,12 +1,14 @@
-const { Pool } = require("pg");
-require("dotenv").config();
+// db/pools.js
+const { Pool } = require('pg');
+require('dotenv').config();
+
+const isProd = process.env.NODE_ENV === 'production';
 
 const pool = new Pool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT
+    connectionString: process.env.DATABASE_URL,
+    ssl: isProd
+        ? { rejectUnauthorized: false }  // Railway (prod) : SSL
+        : false                          // Local : pas de SSL
 });
 
 module.exports = pool;
